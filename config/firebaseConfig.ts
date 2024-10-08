e0b05d9175ffc7a71190c37f6d4abb67e1c27a22
Gaëtan Péron <gaetan.peron456@gmail.com>
2024-10-08 09:33:07 +0200

    Auth page et navigator

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,17 +1,21 @@
 // src/firebaseConfig.ts
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Configuration de Firebase (copiée depuis Firebase Console)
+// Firebase configuration (copied from Firebase Console)
 const firebaseConfig = {
-    apiKey: "AIzaSyAl-agz25C0zyynOANVQs8mPzegv9T0S6A",
-    authDomain: "gestion-de-chantiers-34eed.firebaseapp.com",
-    projectId: "gestion-de-chantiers-34eed",
-    storageBucket: "gestion-de-chantiers-34eed.appspot.com",
-    messagingSenderId: "528326924813",
-    appId: "1:528326924813:web:d528941abe696ef2bf9344",
+  apiKey: "AIzaSyAl-agz25C0zyynOANVQs8mPzegv9T0S6A",
+  authDomain: "gestion-de-chantiers-34eed.firebaseapp.com",
+  projectId: "gestion-de-chantiers-34eed",
+  storageBucket: "gestion-de-chantiers-34eed.appspot.com",
+  messagingSenderId: "528326924813",
+  appId: "1:528326924813:web:d528941abe696ef2bf9344",
 };
 
-// Initialisation de Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Export Firebase Auth and Firestore
+export const FIREBASE_AUTH = getAuth(app);
 export const db = getFirestore(app);
