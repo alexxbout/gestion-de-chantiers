d11123679ac7eba30c7ebe3b4c7db12e1944b7b0
alexxbout <boutinaud.pro@gmail.com>
2024-11-17 15:49:03 +0100

    fixed keyboard issues on login page

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -9,7 +9,7 @@ import { StackNavigationProp } from "@react-navigation/stack";
 import { useNavigation } from "expo-router";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
-import { Pressable, Text, View } from "react-native";
+import { KeyboardAvoidingView, Platform, Pressable, ScrollView, Text, View } from "react-native";
 
 type LoginNavigationProp = StackNavigationProp<RootStackParamList, "login">;
 
@@ -48,48 +48,52 @@ const Login = () => {
     };
 
     return (
-        <View className="w-screen h-screen">
-            <Image className="flex-1 w-full" alt="login" source={{ uri: "https://images.unsplash.com/photo-1518005020951-eccb494ad742?q=80&w=2965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }} />
-            <View className="flex justify-between w-full p-10 bg-white h-max gap-y-20">
-                <Text className="text-4xl font-bold">{ isLogin ? "Bonjour !" : "Créez votre compte."}</Text>
+        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={{ flex: 1 }}>
+            <ScrollView contentContainerStyle={{ flexGrow: 1 }} keyboardShouldPersistTaps="handled">
+                <View className="w-screen h-screen">
+                    <Image className="flex-1 w-full" alt="login" source={{ uri: "https://images.unsplash.com/photo-1518005020951-eccb494ad742?q=80&w=2965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }} />
+                    <View className="flex justify-between w-full p-10 bg-white h-max gap-y-20">
+                        <Text className="text-4xl font-bold">{isLogin ? "Bonjour !" : "Créez votre compte."}</Text>
 
-                <FormControl className="flex flex-col items-center justify-center w-full gap-y-5" isInvalid={isInvalid}>
-                    <Box className="flex flex-col w-full gap-y-2">
-                        <Text className="text-gray-500">Email</Text>
-                        <Input className="w-full" variant="outline" size="xl">
-                            <InputField placeholder="Email" onChangeText={(text) => setEmail(text)} />
-                        </Input>
-                    </Box>
-                    <Box className="flex flex-col w-full gap-y-2">
-                        <Text className="text-gray-500">Mot de passe</Text>
-                        <Input className="w-full" variant="outline" size="xl">
-                            <InputField type={showPassword ? "text" : "password"} placeholder="Mot de passe" onChangeText={(text) => setPassword(text)} />
-                            <InputSlot className="pr-2" onPress={handleState}>
-                                <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} size="md" />
-                            </InputSlot>
-                        </Input>
-                    </Box>
+                        <FormControl className="flex flex-col items-center justify-center w-full gap-y-5" isInvalid={isInvalid}>
+                            <Box className="flex flex-col w-full gap-y-2">
+                                <Text className="text-gray-500">Email</Text>
+                                <Input className="w-full" variant="outline" size="xl">
+                                    <InputField placeholder="Email" onChangeText={(text) => setEmail(text)} />
+                                </Input>
+                            </Box>
+                            <Box className="flex flex-col w-full gap-y-2">
+                                <Text className="text-gray-500">Mot de passe</Text>
+                                <Input className="w-full" variant="outline" size="xl">
+                                    <InputField type={showPassword ? "text" : "password"} placeholder="Mot de passe" onChangeText={(text) => setPassword(text)} />
+                                    <InputSlot className="pr-2" onPress={handleState}>
+                                        <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} size="md" />
+                                    </InputSlot>
+                                </Input>
+                            </Box>
 
-                    <FormControlError>
-                        <FormControlErrorIcon as={AlertCircleIcon} />
-                        <FormControlErrorText>{errorMessage}</FormControlErrorText>
-                    </FormControlError>
-                </FormControl>
+                            <FormControlError>
+                                <FormControlErrorIcon as={AlertCircleIcon} />
+                                <FormControlErrorText>{errorMessage}</FormControlErrorText>
+                            </FormControlError>
+                        </FormControl>
 
-                <Box className="flex flex-col w-full gap-y-5">
-                    <Pressable onPress={handleAuthAction} className={isButtonDisabled ? "p-3 bg-gray-300 rounded-md" : "p-3 bg-black rounded-md"} disabled={isButtonDisabled}>
-                        <Text className="text-xl font-semibold text-center text-white">{isLogin ? "Connexion" : "S'enregistrer"}</Text>
-                    </Pressable>
+                        <Box className="flex flex-col w-full gap-y-5">
+                            <Pressable onPress={handleAuthAction} className={isButtonDisabled ? "p-3 bg-gray-300 rounded-md" : "p-3 bg-black rounded-md"} disabled={isButtonDisabled}>
+                                <Text className="text-xl font-semibold text-center text-white">{isLogin ? "Connexion" : "S'enregistrer"}</Text>
+                            </Pressable>
 
-                    <Text className="text-center">
-                        {isLogin ? "Vous n'avez pas de compte ? " : "Déjà un compte ? "}
-                        <Text className="font-bold text-blue-600" onPress={() => setIsLogin(!isLogin)}>
-                            {isLogin ? "S'enregistrer" : "Se connecter"}
-                        </Text>
-                    </Text>
-                </Box>
-            </View>
-        </View>
+                            <Text className="text-center">
+                                {isLogin ? "Vous n'avez pas de compte ? " : "Déjà un compte ? "}
+                                <Text className="font-bold text-blue-600" onPress={() => setIsLogin(!isLogin)}>
+                                    {isLogin ? "S'enregistrer" : "Se connecter"}
+                                </Text>
+                            </Text>
+                        </Box>
+                    </View>
+                </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 };
 
