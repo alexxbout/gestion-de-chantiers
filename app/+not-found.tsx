ace04bd00f8c3033e277aa57f7ca0094944983ca
alexxbout <boutinaud.pro@gmail.com>
2024-11-17 15:41:57 +0100

    updated not found

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,10 +1,17 @@
+import { Button, ButtonText } from "@/components/ui/button";
 import { Text } from "@/components/ui/text";
+import { useRouter } from "expo-router";
 import { View } from "react-native";
 
 const Layout = () => {
+    const router = useRouter();
+
     return (
-        <View>
-            <Text>Page not found</Text>
+        <View className="flex items-center justify-center w-screen h-screen">
+            <Text className="font-semibold">Oups ! On dirait que cette page n'exsite pas...</Text>
+            <Button onPress={() => router.navigate("/(tabs)/worksites")} variant="link">
+                <ButtonText className="text-blue-600">Retourner à l'accueil</ButtonText>
+            </Button>
         </View>
     );
 };
