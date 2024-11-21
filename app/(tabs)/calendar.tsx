8da9f520c8370eea8649f93e9f8589e66ab75b0d
alexandre boutinaud <aboutinaud@e206m08.istic.univ-rennes1.fr>
2024-11-21 14:18:24 +0100

    added calendar

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -1,7 +1,77 @@
-import { View } from "react-native";
+import { View } from "@/components/ui/view";
+import { useState } from "react";
+import {
+  Calendar,
+  CalendarList,
+  Agenda,
+  LocaleConfig,
+} from "react-native-calendars";
+
+LocaleConfig.locales["fr"] = {
+  monthNames: [
+    "Janvier",
+    "Février",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Août",
+    "Septembre",
+    "Octobre",
+    "Novembre",
+    "Décembre",
+  ],
+  monthNamesShort: [
+    "Janv.",
+    "Févr.",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juil.",
+    "Août",
+    "Sept.",
+    "Oct.",
+    "Nov.",
+    "Déc.",
+  ],
+  dayNames: [
+    "Dimanche",
+    "Lundi",
+    "Mardi",
+    "Mercredi",
+    "Jeudi",
+    "Vendredi",
+    "Samedi",
+  ],
+  dayNamesShort: ["Dim.", "Lun.", "Mar.", "Mer.", "Jeu.", "Ven.", "Sam."],
+  today: "Aujourd'hui",
+};
+
+LocaleConfig.defaultLocale = "fr";
 
 const Tab = () => {
-    return <View className="flex flex-col h-full p-5 bg-white gap-y-10"></View>;
-}
+  const [selected, setSelected] = useState("");
+
+  return (
+    <View className="flex flex-col h-full p-5 bg-white gap-y-10">
+      <Calendar
+        firstDay={1}
+        enableSwipeMonths={true}
+        onDayPress={(day: any) => {
+          setSelected(day.dateString);
+        }}
+        markedDates={{
+          [selected]: {
+            selected: true,
+            disableTouchEvent: true,
+            selectedDotColor: "orange",
+          },
+        }}
+      />
+    </View>
+  );
+};
 
 export default Tab;
