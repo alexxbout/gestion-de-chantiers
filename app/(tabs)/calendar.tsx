b27ffd399dc176efa28f6d0b7069615a4596b7ff
alexxbout <boutinaud.pro@gmail.com>
2024-11-25 13:45:22 +0100

    updated calendar with worksite card

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -1,77 +1,82 @@
+import WorksiteCardSmall from "@/components/custom/worksite-card-small";
+import { Text } from "@/components/ui/text";
 import { View } from "@/components/ui/view";
+import { WorksiteStatus } from "@/types/database";
+import { WorksiteProp } from "@/types/navigation";
 import { useState } from "react";
-import {
-  Calendar,
-  CalendarList,
-  Agenda,
-  LocaleConfig,
-} from "react-native-calendars";
+import { Calendar, LocaleConfig } from "react-native-calendars";
+import { ScrollView } from "react-native-gesture-handler";
 
 LocaleConfig.locales["fr"] = {
-  monthNames: [
-    "Janvier",
-    "Février",
-    "Mars",
-    "Avril",
-    "Mai",
-    "Juin",
-    "Juillet",
-    "Août",
-    "Septembre",
-    "Octobre",
-    "Novembre",
-    "Décembre",
-  ],
-  monthNamesShort: [
-    "Janv.",
-    "Févr.",
-    "Mars",
-    "Avril",
-    "Mai",
-    "Juin",
-    "Juil.",
-    "Août",
-    "Sept.",
-    "Oct.",
-    "Nov.",
-    "Déc.",
-  ],
-  dayNames: [
-    "Dimanche",
-    "Lundi",
-    "Mardi",
-    "Mercredi",
-    "Jeudi",
-    "Vendredi",
-    "Samedi",
-  ],
-  dayNamesShort: ["Dim.", "Lun.", "Mar.", "Mer.", "Jeu.", "Ven.", "Sam."],
-  today: "Aujourd'hui",
+    monthNames: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
+    monthNamesShort: ["Janv.", "Févr.", "Mars", "Avril", "Mai", "Juin", "Juil.", "Août", "Sept.", "Oct.", "Nov.", "Déc."],
+    dayNames: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"],
+    dayNamesShort: ["Dim.", "Lun.", "Mar.", "Mer.", "Jeu.", "Ven.", "Sam."],
+    today: "Aujourd'hui",
 };
 
 LocaleConfig.defaultLocale = "fr";
 
 const Tab = () => {
-  const [selected, setSelected] = useState("");
+    const [selected, setSelected] = useState("");
 
-  return (
-    <View className="flex flex-col h-full p-5 bg-white gap-y-10">
-      <Calendar
-        firstDay={1}
-        enableSwipeMonths={true}
-        onDayPress={(day: any) => {
-          setSelected(day.dateString);
-        }}
-        markedDates={{
-          [selected]: {
-            selected: true,
-            disableTouchEvent: true,
-            selectedDotColor: "orange",
-          },
-        }}
-      />
-    </View>
-  );
+    const worksites: WorksiteProp[] = [
+        {
+            id: 1,
+            title: "Les Terrasses",
+            description: "Projet résidentiel avec des espaces en terrasses ou en hauteur",
+            start_date: "2022-01-01",
+            status: WorksiteStatus.COMPLETED,
+        },
+        {
+            id: 2,
+            title: "Cœur de Ville",
+            description: "Revitalisation urbaine en centre-ville",
+            start_date: "2022-01-01",
+            status: WorksiteStatus.IN_PROGRESS,
+        },
+        {
+            id: 3,
+            title: "Chantier 3",
+            description: "Description du chantier 3",
+            start_date: "2022-01-01",
+            status: WorksiteStatus.INTERRUPTED,
+        },
+        {
+            id: 4,
+            title: "Chantier 4",
+            description: "Description du chantier 3",
+            start_date: "2022-01-01",
+            status: WorksiteStatus.INTERRUPTED,
+        },
+    ];
+
+    return (
+        <ScrollView className="flex flex-col h-full p-5 pb-20 bg-white">
+            <Calendar
+                firstDay={1}
+                enableSwipeMonths={true}
+                onDayPress={(day: any) => {
+                    setSelected(day.dateString);
+                }}
+                markedDates={{
+                    [selected]: {
+                        selected: true,
+                        disableTouchEvent: true,
+                        selectedDotColor: "orange",
+                    },
+                }}
+            />
+
+            <View className="flex flex-col gap-y-5">
+                <Text className="pt-10 text-2xl font-medium text-black">{(worksites.length > 0 ? worksites.length : "Aucun") + " chantier" + (worksites.length > 0 ? "s" : "") + " ce jour"}</Text>
+
+                {worksites.map((worksite) => (
+                    <WorksiteCardSmall key={worksite.id} {...worksite} />
+                ))}
+            </View>
+        </ScrollView>
+    );
 };
 
 export default Tab;
---
+++ components/custom/worksite-card-small.tsx
@@ -0,0 +1,32 @@
+import { WorksiteProp } from "@/types/navigation";
+import { useRouter } from "expo-router";
+import { Pressable, View } from "react-native";
+import { Button, ButtonText } from "../ui/button";
+import { Text } from "../ui/text";
+import { formatDate } from "../utils";
+
+const WorksiteCardSmall = (props: WorksiteProp) => {
+    const router = useRouter();
+
+    const showInfo = (id: number) => {
+        router.push({ pathname: "/worksites/[id]", params: { id: id.toString() } });
+    };
+
+    return (
+        <Pressable onPress={() => showInfo(props.id)} className="flex flex-col p-3 rounded-md gap-y-5 bg-card">
+            <Text className="text-xl font-semibold text-black">{props.title}</Text>
+
+            <View className="flex flex-row justify-between gap-x-5">
+                <View className="flex w-full gap-y-2 shrink">
+                    <Text className="overflow-hidden truncate whitespace-nowrap">{props.description}</Text>
+                    <Text>{formatDate(props.start_date)}</Text>
+                </View>
+                <Button onPress={() => showInfo(props.id)} className="text-white w-max">
+                    <ButtonText>Détails</ButtonText>
+                </Button>
+            </View>
+        </Pressable>
+    );
+};
+
+export default WorksiteCardSmall;
