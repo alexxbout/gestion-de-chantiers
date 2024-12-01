470426d173f85330f65e7b6a78542fc91185f564
alexxbout <boutinaud.pro@gmail.com>
2024-12-01 15:17:33 +0100

    added calendar

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -1,15 +1,16 @@
 import WorksiteCardSmall from "@/components/custom/worksite-card-small";
 import { Text } from "@/components/ui/text";
 import { View } from "@/components/ui/view";
-import { WorksiteStatus } from "@/types/database";
-import { WorksiteProp } from "@/types/navigation";
-import { useState } from "react";
+import { getAllDocuments } from "@/config/firebaseConfig";
+import { CollectionName, Worksite } from "@/types/database";
+import { useFocusEffect } from "expo-router";
+import { useCallback, useState } from "react";
 import { Calendar, LocaleConfig } from "react-native-calendars";
 import { ScrollView } from "react-native-gesture-handler";
 
 LocaleConfig.locales["fr"] = {
     monthNames: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
-    monthNamesShort: ["Janv.", "Févr.", "Mars", "Avril", "Mai", "Juin", "Juil.", "Août", "Sept.", "Oct.", "Nov.", "Déc."],
+    monthNamesShort: ["Janv.", "Févr.", "Mars", "Avril", "Mai", "Juin", "Juil.", "Août.", "Sept.", "Oct.", "Nov.", "Déc."],
     dayNames: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"],
     dayNamesShort: ["Dim.", "Lun.", "Mar.", "Mer.", "Jeu.", "Ven.", "Sam."],
     today: "Aujourd'hui",
@@ -19,37 +20,69 @@ LocaleConfig.defaultLocale = "fr";
 
 const Tab = () => {
     const [selected, setSelected] = useState("");
+    const [worksites, setWorksites] = useState<Worksite[]>([]);
+    const [worksiteColors, setWorksiteColors] = useState<{ [id: number]: string }>({});
+    const [markedDates, setMarkedDates] = useState({});
 
-    const worksites: WorksiteProp[] = [
-        {
-            id: 1,
-            title: "Les Terrasses",
-            description: "Projet résidentiel avec des espaces en terrasses ou en hauteur",
-            start_date: "2022-01-01",
-            status: WorksiteStatus.COMPLETED,
-        },
-        {
-            id: 2,
-            title: "Cœur de Ville",
-            description: "Revitalisation urbaine en centre-ville",
-            start_date: "2022-01-01",
-            status: WorksiteStatus.IN_PROGRESS,
-        },
-        {
-            id: 3,
-            title: "Chantier 3",
-            description: "Description du chantier 3",
-            start_date: "2022-01-01",
-            status: WorksiteStatus.INTERRUPTED,
-        },
-        {
-            id: 4,
-            title: "Chantier 4",
-            description: "Description du chantier 3",
-            start_date: "2022-01-01",
-            status: WorksiteStatus.INTERRUPTED,
-        },
-    ];
+    const fetchWorksites = async () => {
+        try {
+            const fetchedWorksites = await getAllDocuments<Worksite[]>(CollectionName.WORKSITE);
+
+            // Générer des couleurs distinctes
+            const colorsArray = generateDistinctColors(fetchedWorksites.flat().length);
+            const colors: { [id: number]: string } = {};
+
+            // Générer les périodes marquées
+            const dates: { [key: string]: { periods: { startingDay: boolean; endingDay: boolean; color: string }[] } } = {};
+            fetchedWorksites.flat().forEach((worksite, index) => {
+                const worksiteColor = colorsArray[index];
+                colors[worksite.id] = worksiteColor;
+
+                // Calcul des périodes
+                const startDate = new Date(worksite.startDate);
+                for (let i = 0; i <= worksite.duration; i++) {
+                    const currentDate = new Date(startDate);
+                    currentDate.setDate(currentDate.getDate() + i);
+
+                    const formattedDate = currentDate.toISOString().split("T")[0];
+
+                    if (!dates[formattedDate]) {
+                        dates[formattedDate] = { periods: [] };
+                    }
+
+                    dates[formattedDate].periods.push({
+                        startingDay: i === 0,
+                        endingDay: i === worksite.duration,
+                        color: worksiteColor,
+                    });
+                }
+            });
+
+            setMarkedDates(dates);
+            setWorksites(fetchedWorksites.flat());
+            setWorksiteColors(colors);
+        } catch (error) {
+            console.error("Error fetching worksites: ", error);
+        }
+    };
+
+    const generateDistinctColors = (numColors: number): string[] => {
+        const colors = [];
+        const step = 360 / numColors; // Division équidistante sur le cercle chromatique
+
+        for (let i = 0; i < numColors; i++) {
+            const hue = i * step; // Calcul de la teinte
+            colors.push(`hsl(${hue}, 80%, 50%)`); // Saturation élevée, luminosité modérée
+        }
+
+        return colors;
+    };
+
+    useFocusEffect(
+        useCallback(() => {
+            fetchWorksites();
+        }, [])
+    );
 
     return (
         <ScrollView className="flex flex-col h-full p-5 pb-20 bg-white">
@@ -59,7 +92,9 @@ const Tab = () => {
                 onDayPress={(day: any) => {
                     setSelected(day.dateString);
                 }}
+                markingType="multi-period"
                 markedDates={{
+                    ...markedDates,
                     [selected]: {
                         selected: true,
                         disableTouchEvent: true,
@@ -72,7 +107,7 @@ const Tab = () => {
                 <Text className="pt-10 text-2xl font-medium text-black">{(worksites.length > 0 ? worksites.length : "Aucun") + " chantier" + (worksites.length > 0 ? "s" : "") + " ce jour"}</Text>
 
                 {worksites.map((worksite) => (
-                    <WorksiteCardSmall key={worksite.id} {...worksite} />
+                    <WorksiteCardSmall key={worksite.id} worksite={worksite} color={worksiteColors[worksite.id]} />
                 ))}
             </View>
         </ScrollView>
--- components/custom/worksite-card-small.tsx
+++ components/custom/worksite-card-small.tsx
@@ -1,11 +1,11 @@
-import { WorksiteProp } from "@/types/navigation";
+import { Worksite } from "@/types/database";
 import { useRouter } from "expo-router";
 import { Pressable, View } from "react-native";
 import { Button, ButtonText } from "../ui/button";
 import { Text } from "../ui/text";
 import { formatDate } from "../utils";
 
-const WorksiteCardSmall = (props: WorksiteProp) => {
+const WorksiteCardSmall = (props: {worksite: Worksite, color: string}) => {
     const router = useRouter();
 
     const showInfo = (id: number) => {
@@ -13,15 +13,15 @@ const WorksiteCardSmall = (props: WorksiteProp) => {
     };
 
     return (
-        <Pressable onPress={() => showInfo(props.id)} className="flex flex-col p-3 rounded-md gap-y-5 bg-card">
-            <Text className="text-xl font-semibold text-black">{props.title}</Text>
+        <Pressable onPress={() => showInfo(props.worksite.id)} className="flex flex-col p-3 rounded-md gap-y-5 bg-card" style={{borderColor: props.color, borderWidth: 2}}>
+            <Text className="text-xl font-semibold text-black">{props.worksite.title}</Text>
 
             <View className="flex flex-row items-end justify-between gap-x-5">
                 <View className="flex w-full gap-y-2 shrink">
-                    <Text className="overflow-hidden truncate whitespace-nowrap">{props.description}</Text>
-                    <Text>{formatDate(props.start_date)}</Text>
+                    <Text className="overflow-hidden truncate whitespace-nowrap">{props.worksite.description}</Text>
+                    <Text>{formatDate(props.worksite.startDate)}</Text>
                 </View>
-                <Button onPress={() => showInfo(props.id)} className="text-white w-max">
+                <Button onPress={() => showInfo(props.worksite.id)} className="text-white w-max">
                     <ButtonText>Détails</ButtonText>
                 </Button>
             </View>
