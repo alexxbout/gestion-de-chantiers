603e8033e54dd99fd6f4c7075737990dd00a3171
alexxbout <boutinaud.pro@gmail.com>
2024-12-05 15:19:48 +0100

    updated readme

--- clean.sh
+++ clean.sh
@@ -9,3 +9,14 @@ npx gluestack-ui init
 npx gluestack-ui@latest add --all
 
 # npx expo start --clear
+
+# npm cache clean --force
+# watchman watch-del-all
+# npm i
+# npx gluestack-ui init
+# npx gluestack-ui@latest add --all
+# npx expo start --clear
+
+# npm cache clean --force; watchman watch-del-all; npm i; npx gluestack-ui init; npx gluestack-ui@latest add --all; npx expo start --clear
+
+# npx gluestack-ui@latest add --all; npm cache clean --force; npx expo start --clear
