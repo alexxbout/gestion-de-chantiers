26b8d71b1bd22504444f9f3eb6bd83753fa7b273
alexxbout <boutinaud.pro@gmail.com>
2024-12-05 21:06:52 +0100

    updated gluestack components

--- clean.sh
+++ clean.sh
@@ -2,21 +2,4 @@
 
 rm -rf .expo node_modules package-lock.json tailwind.config.js gluestack-ui.config.json nativewind-env.d.ts global.css components/ui
 
-npm cache clean --force
-watchman watch-del-all
-npm i
-npx gluestack-ui init
-npx gluestack-ui@latest add --all
-
-# npx expo start --clear
-
-# npm cache clean --force
-# watchman watch-del-all
-# npm i
-# npx gluestack-ui init
-# npx gluestack-ui@latest add --all
-# npx expo start --clear
-
-# npm cache clean --force; watchman watch-del-all; npm i; npx gluestack-ui init; npx gluestack-ui@latest add --all; npx expo start --clear
-
-# npx gluestack-ui@latest add --all; npm cache clean --force; npx expo start --clear
+npm cache clean --force; watchman watch-del-all; npm i; npx gluestack-ui init; npx gluestack-ui@latest add --all; npx expo start --clear
